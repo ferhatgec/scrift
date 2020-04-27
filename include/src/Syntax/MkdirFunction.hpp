9e013fbc8ff8e04e1080adf7b53c5d0ce8c596c1
FerhatGec <ferhatgectao@gmail.com>
2020-04-27 23:09:24 +0300

    Added MkdirFunction

---
+++ include/src/Syntax/MkdirFunction.hpp
@@ -0,0 +1,12 @@
+#ifndef MKDIR_FUNCTION_HPP
+#define MKDIR_FUNCTION_HPP
+#include "../synflang.hpp"
+#include "../main.h"
+#include "PrintErrorFunction.hpp"
+
+fprinterror *error = new fprinterror;
+typedef struct {
+
+} fmkdir;
+
+#endif // MKDIR_FUNCTION_HPP
