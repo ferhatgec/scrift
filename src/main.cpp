dbd78a4b26565132247132a8e4fb49e6ed848a1c
FerhatGec <ferhatgectao@gmail.com>
2020-04-28 23:08:09 +0300

    Rechange while command

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@
 #include "../include/src/Syntax/KernelName.hpp"
 #include "../include/src/Syntax/HelpFunction.hpp"
 #include "../include/src/Syntax/GetNameFunction.hpp"
+
 // Variables 
 static integer argc;
 static fchar* argv[128];
@@ -45,6 +46,12 @@ fkernel *kernel = new fkernel;
 FCDFunction *cdfunction = new FCDFunction();
 FMKDirFunction *mkdirfunction = new FMKDirFunction();
 FCreateFileFunction *filefunction = new FCreateFileFunction();
+FGetUsername *userhostname = new FGetUsername(); 
+
+// TEST VARIABLES 
+fchar* testcharacter = new fchar;
+integer test;
+
 FMain::FMain()
 {
 
@@ -54,7 +61,7 @@ FMain::FMain()
 FMain::~FMain()
 {
     delete terminalstr,  helpstr, kernel,  _username, _os_kernel_, _run_file, _run_file_, _file_def_path,
-    _ech_str, _h_str, argv, main_, mkdirfunction, filefunction;
+    _ech_str, _h_str, argv, main_, mkdirfunction, filefunction, userhostname;
 }   
 
 
@@ -67,41 +74,48 @@ FMain::usage()
        "<arguments> -> var\n");
 }
 
+
 void
 FMain::Shell()
 {
-    helpstr->hello();
-    while(argc == 0)
-    {
+    testa:test = 1;
     argc = 0;
+
+    
     terminalstr->Terminal();
-    str = _h_str;
-    std::cin >> _h_str;
+    std::cin >> _h_str; // ws -> whitespace
+    test = 2;
+    slashn
     if(_h_str == "help" || _h_str == "-h")
     {
         argc = 1;
         helpstr->help();
         argc = 0;
+        test = 1;
     }
     else if(_h_str == "mkdir" || _h_str == "createdir") {
         argc = 1;
         mkdirfunction->MKDirFunctionInit();
         argc = 0;
+        test = 1;
     }
-    else if(_h_str == "clear" || _h_str == "clear!!!") {argc = 1; main_->clear_shell(); argc = 0;}
-    else if (_h_str == "home" || _h_str == "default") {argc = 1;  argc = 0; }
-    else if(_h_str == "echo" || _h_str == "printlnf"){argc = 1; main_->echo_printlnf(); argc = 0;}
-    else if(_h_str == "cd" || _h_str == "opendir") {argc = 1; cdfunction->CDFunctionInit(); argc = 0;}
+    else if(_h_str == "clear" || _h_str == "clear!!!") {argc = 1; main_->clear_shell(); argc = 0;
+    test = 1;}
+    else if (_h_str == "home" || _h_str == "default") {argc = 1;  argc = 0; 
+    test = 1;}
+    else if(_h_str == "echo" || _h_str == "printlnf"){argc = 1; main_->echo_printlnf(); argc = 0;  test = 1;}
+    else if(_h_str == "cd" || _h_str == "opendir") {argc = 1; cdfunction->CDFunctionInit(); argc = 0;  test = 1;}
     else if(_h_str == "plus") {argc = 1; main_->plus_num(fn, sn); argc = 0;} 
     else if(_h_str == "brk" || _h_str == "cls")  {exit(EXIT_SUCCESS);} 
-    else if(_h_str == "username" || _h_str == "uname") {argc = 1;  printf("\n"); argc = 0;} 
+    else if(_h_str == "username" || _h_str == "uname") {argc = 1;  userhostname->InitUsername(); argc = 0;  test = 1;} 
     else if(_h_str == "ls" || _h_str == "dir") // list directory
     {
        argc = 1;
        fchar *_char_cin;
        main_->list_dir(true, true, true); 
        slashn
-       argc = 0;    
+       argc = 0; 
+       test = 1;   
     }
     else if(_h_str == "lsf" || _h_str == "lsfile")
     {   
@@ -109,6 +123,7 @@ FMain::Shell()
         main_->list_file(true);
         slashn
         argc = 0;
+        test = 1;
     }
     else if(_h_str == "scr" || _h_str == "scriftrun")
     {
@@ -121,17 +136,22 @@ FMain::Shell()
         argc = 1;
         main_->list_direc(true);
         slashn
-        argc = 0;
+        argc = 0;  test = 1;
     }
-    else if(_h_str == "randomizestr" || _h_str == "rstr"){argc = 1; main_->_generated_hash_string(x); argc = 0;}
-    else if(_h_str == "fetcheya" || _h_str == "-f"){argc = 1; kernel->KernelName(); argc = 0;}
+    else if(_h_str == "randomizestr" || _h_str == "rstr"){argc = 1; main_->_generated_hash_string(x); argc = 0;  test = 1;}
+    else if(_h_str == "fetcheya" || _h_str == "-f"){argc = 1; kernel->KernelName(); argc = 0;  test = 1; }
     else if(_h_str == "ctxt" || _h_str == "createtxt"){
         argc = 1;
         filefunction->CreateFileFunctionInit();
-        argc = 0;
+        argc = 0;  test = 1;
     }
-    else if(_h_str  == "setlocale_system" || _h_str == "slcl_sys"){argc = 1; main_->_set_locale(); argc = 0;}  
-    else if(_h_str == "ip" || _h_str == "myip"){argc = 1; main_->_your_ip(); argc = 0;} 
+    else if(_h_str  == "setlocale_system" || _h_str == "slcl_sys"){argc = 1; main_->_set_locale(); argc = 0;  test = 1;}  
+    else if(_h_str == "ip" || _h_str == "myip"){argc = 1; main_->_your_ip(); argc = 0;
+     test = 1;} 
+    else {
+        printlnf("This command is not found!");
+        slashn 
+        test = 3;
     }
 }
 
@@ -140,12 +160,11 @@ FMain::Shell()
 integer main(integer argc, fchar* argv[])
 {
     std::shared_ptr<FMain> fmain = std::make_shared<FMain>(); 
-    if(argc < 2)
-        fmain->usage();
-
-
-    fmain->Shell();
-    return F_OK;
+    helpstr->hello();
+    while(argc = 2) {    
+        fmain->Shell();
+    }
+    return 0;
 }
 
 
