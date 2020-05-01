65329bc11c7fd97391bfcbfcf4ae16fc0bb4a269
FerhatGec <ferhatgectao@gmail.com>
2020-05-02 02:23:35 +0300

    Added ValueClass

--- include/src/Language/VM/Value.hpp
+++ include/src/Language/VM/Value.hpp
@@ -12,7 +12,7 @@
 #include <sstream>
 #include <unordered_map>
 #include <vector>
-
+#include "../../main.h"
 class VM;
 
 enum class TypeFunction {
@@ -24,6 +24,42 @@ enum class TypeFunction {
     FUNCTION,
     CLASS,
     NAME,
+};
+
+class Value {
+    typedef Value* (*Method)(Value*, std::vector<Value*>);
+    typedef std::unordered_map<std::string, Method> Methods;
+    protected:
+        union  
+        {
+           double number;
+           std::string* strvalue;
+           Methods* methods;
+           Method single_method;
+        } data;
+        
+    public:
+        TypeFunction type;
+        Value();
+        Value(TypeFunction);
+        Value(TypeFunction, std::string);
+        Value(TypeFunction, double);
+
+        virtual void SetTypeFunc(TypeFunction);
+        std::string Print(boolean  printtype = false);
+        Value* CloneValue();
+
+        std::string GetString();
+        double GetNumber();
+        boolean GetBoolean();
+
+        void Init() {};
+
+        Value* exec_method(std:string,
+        std::vecotr<Value*>);
+
+        func AddMethod(std::string, Method);
+        boolean HasMethod(std::string);
 };
 
 #endif // VALUE_HPP
