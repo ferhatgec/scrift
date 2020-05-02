57fd3bda4d971581bada0d270ce6e0b4d08d4a26
FerhatGec <ferhatgectao@gmail.com>
2020-05-02 03:15:19 +0300

    Added Next Function..

--- src/Scrift/Lexer.cpp
+++ src/Scrift/Lexer.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <string>
 #include <Scrift/Lexer.hpp>
-
+#include <main.h>
 
 FLexer::FLexer() {
 	// Initialize all operators and keywords
@@ -46,3 +46,81 @@ FLexer::FLexer() {
 	keywords["for"] = true;
 	keywords["in"] = true;
 }
+
+std::vector<Token> FLexer::parse_file() {
+	std::vector<Token> result;
+    
+	std::ifstream file("test.scr"); // For testing
+
+    while (std::getline(file, this->row)) // GetLine input.
+    {
+    	std::cout << this->row << std::endl;
+
+    	this->index = 0;
+
+    	while (true) {
+        	Token tok = this->next(); 
+        	tok.print();
+        	this->index++;
+
+        	if (tok.type == FType::T_EOF) {
+        		break;
+        	}
+        }
+    }
+
+    return result;
+}
+
+char FLexer::char_at_pos(integer index) {
+	if (index >= this->row.size()) {
+		return '\0';
+	}
+
+	return this->row[index];
+}
+
+Token FLexer::next() { 
+	this->current = this->char_at_pos(this->index);
+
+	std::cout << "next(): " << this->current;
+    slashn
+    
+	if (this->current == '\0') {
+		return Token::T_EOF();
+	}
+
+	// Line endings
+	if (this->current == '\n' || this->current == '\r') {
+		return Token::T_EOL();
+	}
+
+	// Ignore Whitespace
+	if (this->current == ' ') {
+		return Token::IGNORE();
+	}
+
+	// Comments
+	if (this->current == '/' && this->char_at_pos(this->index+1) == '/') {
+		return this->comment();
+	}
+
+	// Names
+	// Begins with a char a-Z
+	if ((this->current >= 'a' && this->current <= 'z') || (this->current >= 'A' && this->current <= 'Z')) {
+		return this->name();
+	}
+
+	// Numbers
+	if (this->current >= '0' && this->current <= '9') {
+		return this->number();
+	}
+
+	// Strings
+	if (this->current == '\"') {
+		return this->string();
+	}
+
+	return Token::IGNORE();
+}
+
