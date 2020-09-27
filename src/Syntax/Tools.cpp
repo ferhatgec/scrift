fcd694a6f84cff9a5024d28658c92f9980ce3bf6
ferhatgec <ferhatgectao@gmail.com>
2020-09-28 00:49:19 +0300

    Support 24h time system.

--- src/Syntax/Tools.cpp
+++ src/Syntax/Tools.cpp
@@ -16,62 +16,62 @@ void FTools::Clock() {
     std::string time = main.Time().erase(0, 11).substr(0, 2);
     int _half_time = atoi(main.Time().substr(14, 17).c_str());
 
-    if(time == "01")
+    if(time == "01" || time == "13")
         if(_half_time >= 30)
             std::cout << "🕜";
         else 
             std::cout << "🕐";
-    else if(time == "02")
+    else if(time == "02" || time == "14")
         if(_half_time >= 30)
             std::cout << "🕝";
         else 
             std::cout << "🕑";
-    else if(time == "03")
+    else if(time == "03" || time == "15")
         if(_half_time >= 30)
             std::cout << "🕞";
         else
             std::cout << "🕒";
-    else if(time == "04")
+    else if(time == "04" || time == "16")
         if(_half_time >= 30)
             std::cout << "🕟";
         else
             std::cout << "🕓";
-    else if(time == "05")
+    else if(time == "05" || time == "17")
         if(_half_time >= 30)
             std::cout << "🕠";
         else 
             std::cout << "🕔";
-    else if(time == "06")
+    else if(time == "06" || time == "18")
         if(_half_time >= 30)
             std::cout << "🕡";
         else 
             std::cout << "🕕";
-    else if(time == "07")
+    else if(time == "07" || time == "19")
         if(_half_time >= 30)
             std::cout << "🕢";
         else 
             std::cout << "🕖";
-    else if(time == "08")
+    else if(time == "08" || time == "20")
         if(_half_time >= 30)
             std::cout << "🕣";
         else 
             std::cout << "🕗";
-    else if(time == "09")
+    else if(time == "09" || time == "21")
         if(_half_time >= 30)
             std::cout << "🕤";
         else 
             std::cout << "🕘";
-    else if(time == "10")
+    else if(time == "10" || time == "22")
         if(_half_time >= 30)
             std::cout << "🕥";
         else 
             std::cout << "🕙";
-    else if(time == "11")
+    else if(time == "11" || time == "23")
         if(_half_time >= 30)
             std::cout << "🕦";
         else 
             std::cout << "🕚";
-    else if(time == "00")
+    else if(time == "00" || time == "12")
         if(_half_time >= 30)
             std::cout << "🕧";
         else 
