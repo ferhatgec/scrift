49a26670b61b0f2ac6b43ba64ffbf248c732d871
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-10-21 22:39:19 +0300

    Fix the grammar error

--- src/Scrift.cpp
+++ src/Scrift.cpp
@@ -1048,7 +1048,7 @@ void CodeExecution(std::string arg) {
                 Wait for enter key input.
             */
             BOLD_BLUE_COLOR
-        	std::cout << "Enter the continue...";
+        	std::cout << "Press enter to contiune.";
         	
             returni:if(std::cin.get() == '\n') {
                 	BOLD_GREEN_COLOR
